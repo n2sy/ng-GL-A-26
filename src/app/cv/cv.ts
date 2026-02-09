8c911c59ab08242945b873fc7302545edcbaaaf6
n2sy <nidhal.jelassi@fsegt.utm.tn>
2026-02-09 15:24:19 +0100

    @if et @for

--- src/app/cv/cv.ts
+++ src/app/cv/cv.ts
@@ -13,7 +13,11 @@ export class Cv {
     tabCandidats : Candidat[] = [
         new Candidat(1, "bart", "simpson", 24, "ingénieur", "bart.jpeg"),
         new Candidat(2, "homer", "simpson", 53, "chef de projet", "homer.png"),
-        new Candidat(3, "blisat", "simpson", 27, "designer", "lisa.png"),
-    ]
+        new Candidat(3, "lisa", "simpson", 27, "designer", "lisa.png"),
+    ];
+    selectedCandidate : Candidat
+    recupererCandidat(cand) {
+        this.selectedCandidate = cand        
+    }
 
 }
