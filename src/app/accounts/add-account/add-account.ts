97595e58fe047582c8ab9329af43109e62dbaf07
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2026-02-20 20:51:07 +0100

    correction exercice accounts

--- src/app/accounts/add-account/add-account.ts
+++ src/app/accounts/add-account/add-account.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-account',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-account.html',
   styleUrl: './add-account.css',
 })
 export class AddAccount {
+  allStatus = ['', 'active', 'inactive', 'unknown'];
+  @Output() eventToHomeAccount = new EventEmitter();
 
+  //   nameValue: string = '';
+  //   statusValue: string = '';
+
+  addHandler(nameValue, statusValue) {
+    console.log({
+      name: nameValue,
+      statut: statusValue,
+    });
+
+    this.eventToHomeAccount.emit({
+      name: nameValue,
+      statut: statusValue,
+    });
+  }
 }
--- src/app/accounts/home-accounts/home-accounts.ts
+++ src/app/accounts/home-accounts/home-accounts.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AddAccount } from '../add-account/add-account';
+import { ItemAccount } from '../item-account/item-account';
 
 @Component({
   selector: 'app-home-accounts',
-  imports: [],
+  imports: [AddAccount, ItemAccount],
   templateUrl: './home-accounts.html',
   styleUrl: './home-accounts.css',
 })
 export class HomeAccounts {
+  tabAccounts = [
+    {
+      name: 'Nidhal Account',
+      statut: 'inactive',
+    },
+    {
+      name: 'Sana Account',
+      statut: 'active',
+    },
+  ];
 
+  addAccount(newAccount) {
+    this.tabAccounts.push(newAccount);
+  }
 }
